fb789a5b221eb125443d2aa325dffba9fafb68fc
Nick Rodriguez <nrodrig1@gmail.com>
2024-01-26 07:32:06 -0700

    first commit

---
+++ app_package/__init__.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from app_package.config import config
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from pytz import timezone
+from datetime import datetime
+from ws_models import Base, engine, login_manager
+from flask_mail import Mail
+import secure
+
+
+if not os.path.exists(os.path.join(os.environ.get('WEB_ROOT'),'logs')):
+    os.makedirs(os.path.join(os.environ.get('WEB_ROOT'), 'logs'))
+
+# timezone 
+def timetz(*args):
+    return datetime.now(timezone('Europe/Paris') ).timetuple()
+
+logging.Formatter.converter = timetz
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+formatter_terminal = logging.Formatter('%(asctime)s:%(filename)s:%(name)s:%(message)s')
+
+logger_init = logging.getLogger('__init__')
+logger_init.setLevel(logging.DEBUG)
+
+file_handler = RotatingFileHandler(os.path.join(os.environ.get('WEB_ROOT'),'logs','__init__.log'), mode='a', maxBytes=5*1024*1024,backupCount=2)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter_terminal)
+
+stream_handler_tz = logging.StreamHandler()
+
+logger_init.addHandler(file_handler)
+logger_init.addHandler(stream_handler)
+
+logging.getLogger('werkzeug').setLevel(logging.DEBUG)
+logging.getLogger('werkzeug').addHandler(file_handler)
+
+logger_init.info(f'--- Starting What Sticks 11 Web---')
+TEMPORARILY_DOWN = "ACTIVE" if os.environ.get('TEMPORARILY_DOWN') == "1" else "inactive"
+logger_init.info(f"- TEMPORARILY_DOWN: {TEMPORARILY_DOWN}")
+logger_init.info(f"- FLASK_CONFIG_TYPE: {os.environ.get('FLASK_CONFIG_TYPE')}")
+
+mail = Mail()
+secure_headers = secure.Secure()
+
+def create_app(config_for_flask = config):
+    app = Flask(__name__)   
+    app.config.from_object(config_for_flask)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+
+    ############################################################################
+    ## create folders for DB_ROOT
+    create_folder(config_for_flask.DB_ROOT)
+    # website files
+    create_folder(config_for_flask.WEBSITE_FILES)
+    create_folder(config_for_flask.DIR_WEBSITE_UTILITY_IMAGES)
+    ############################################################################
+
+    logger_init.info(f"- DB_ROOT: {config_for_flask.DB_ROOT}")
+
+    from app_package.bp_main.routes import bp_main
+    from app_package.bp_users.routes import bp_users
+    from app_package.bp_admin.routes import bp_admin
+    from app_package.bp_error.routes import bp_error
+    from app_package.bp_blog.routes import bp_blog
+
+    app.register_blueprint(bp_main)
+    app.register_blueprint(bp_users)
+    app.register_blueprint(bp_admin)
+    app.register_blueprint(bp_error)
+    app.register_blueprint(bp_blog)
+
+    return app
+
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        logger_init.info(f"created: {folder_path}")
---
+++ app_package/bp_main/routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint
+from flask import render_template, current_app, send_from_directory
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+bp_main = Blueprint('bp_main', __name__)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+formatter_terminal = logging.Formatter('%(asctime)s:%(filename)s:%(name)s:%(message)s')
+
+logger_bp_main = logging.getLogger(__name__)
+logger_bp_main.setLevel(logging.DEBUG)
+
+file_handler = RotatingFileHandler(os.path.join(os.environ.get('WEB_ROOT'),'logs','main_routes.log'), mode='a', maxBytes=5*1024*1024,backupCount=2)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter_terminal)
+
+logger_bp_main.addHandler(file_handler)
+logger_bp_main.addHandler(stream_handler)
+
+
+@bp_main.route("/", methods=["GET","POST"])
+def home():
+    logger_bp_main.info(f"-- in home page route --")
+
+    return render_template('main/home.html')
+
+@bp_main.route("/download_ios", methods=["GET","POST"])
+def download_ios():
+    logger_bp_main.info(f"-- in download_ios route --")
+
+    return render_template('main/download_ios.html')
+
+
+@bp_main.route("/about", methods=["GET","POST"])
+def about():
+    logger_bp_main.info(f"-- in about page route --")
+
+    return render_template('main/about.html')
+
+
+
+# Website Files static data
+@bp_main.route('/website_images/<filename>')
+def website_images(filename):
+    print("-- entered website_images -")
+    print(f"Path to image file: {current_app.config.get('DIR_WEBSITE_UTILITY_IMAGES')}")
+    print(f"image filename: {filename}")
+    return send_from_directory(current_app.config.get('DIR_WEBSITE_UTILITY_IMAGES'), filename)
+
+@bp_main.route('/website_images_favicon/<filename>')
+def website_images_favicon(filename):
+    print("-- entered website_images -")
+    print(f"Path to image file: {os.path.join(current_app.config.get('DIR_WEBSITE_UTILITY_IMAGES'),'Favicon')}")
+    print(f"image filename: {filename}")
+    return send_from_directory(os.path.join(current_app.config.get('DIR_WEBSITE_UTILITY_IMAGES'),"Favicon"), filename)
+
+
+@bp_main.route('/favicon_ico')
+def favicon_ico():
+    print("--- ** Favicon() ** ----")
+    return send_from_directory(current_app.config.get('DIR_WEBSITE_UTILITY_IMAGES'),
+                               'Favicon/favicon.ico', mimetype='image/vnd.microsoft.icon')
